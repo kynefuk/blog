a720754097ef5ab7dbc5b545dfb5802e1c23bcd9
kynefuk <atsuminamihira@gmail.com>
2020-12-16 23:51:01 +0900

    add openapi

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -3,6 +3,18 @@ import { AppProps } from "next/app";
 import { CSSReset } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
 
+import Amplify, { Auth } from "aws-amplify";
+
+Amplify.configure({
+  Auth: {
+    identityPoolId: "xxxx",
+    region: "ap-northeast-1",
+    userPoolId: "xxxx",
+    userPoolWebClientId: "xxxx",
+    authenticationFlowType: "USER_SRP_AUTH",
+  },
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,14 +1,14 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
-export default function Home() {
+function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
@@ -63,3 +63,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default withAuthenticator(Home);
