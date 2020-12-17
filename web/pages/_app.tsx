5b168b79f96191e586596b3e3f0f095d033fa7b1
kynefuk <atsuminamihira@gmail.com>
2020-12-17 23:41:03 +0900

    add amplify, swr

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,16 +1,17 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
 import { CSSReset } from "@chakra-ui/react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Container } from "@chakra-ui/react";
+import Head from "next/head";
 
-import Amplify, { Auth } from "aws-amplify";
+import Amplify from "aws-amplify";
 
 Amplify.configure({
   Auth: {
-    identityPoolId: "xxxx",
     region: "ap-northeast-1",
-    userPoolId: "xxxx",
-    userPoolWebClientId: "xxxx",
+    // identityPoolId: process.env.NEXT_PUBLIC_ID_POOL_ID,
+    userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
+    userPoolWebClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID,
     authenticationFlowType: "USER_SRP_AUTH",
   },
 });
@@ -19,7 +20,12 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <CSSReset />
-      <Component {...pageProps} />
+      <Head>
+        <title>My Blog</title>
+      </Head>
+      <Container maxWidth="xl" centerContent>
+        <Component {...pageProps} />
+      </Container>
     </ChakraProvider>
   );
 }
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,66 +1,41 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import { GetServerSideProps } from "next";
 import { withAuthenticator } from "@aws-amplify/ui-react";
+import { withSSRContext, Auth } from "aws-amplify";
+import { Box, Text } from "@chakra-ui/react";
+import { DefaultApi } from "../openapi/api";
+import useSWR from "swr";
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   const SSR = withSSRContext(context);
+//   console.log(context);
+//   try {
+//     // const user = Auth.signIn();
+//   } catch (error) {
+//     console.log(error);
+//   }
+//   return {
+//     props: {},
+//   };
+// };
 
 function Home() {
+  const api = new DefaultApi();
+  const { data, error } = useSWR("/", api.listBlogBlogsGet);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+    <>
+      {data ? (
+        <>
+          {data.data.blogs?.map((blog) => {
+            <Box w="100%" bg="tomato" color="white">
+              <Text align="center">{blog.title}</Text>
+            </Box>;
+          })}
+        </>
+      ) : (
+        <h1>ブログがありません</h1>
+      )}
+    </>
   );
 }
 
